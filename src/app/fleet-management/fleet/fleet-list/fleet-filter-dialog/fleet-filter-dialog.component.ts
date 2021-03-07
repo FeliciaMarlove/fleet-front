f0924c59ba6c83881e2f93049f5916bcb7dcdc33
FeliciaMarlove <missflorencemary@hotmail.com>
2021-03-07 11:49:21 +0100

    feat(FleetFilterDialog) fleet filter done

--- src/app/fleet-management/fleet/fleet-list/fleet-filter-dialog/fleet-filter-dialog.component.ts
+++ src/app/fleet-management/fleet/fleet-list/fleet-filter-dialog/fleet-filter-dialog.component.ts
@@ -1,8 +1,9 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Brand} from '../../../../shared/enums/brand.enum';
 import {FuelType} from '../../../../shared/enums/fuel-type.enum';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {MatSelectChange} from '@angular/material/select';
 
 @Component({
   selector: 'app-fleet-filter-dialog',
@@ -14,9 +15,11 @@ export class FleetFilterDialogComponent implements OnInit {
   public filtersList: {key: string, value: string}[] = [];
   public brandEnum = Brand;
   public fuelEnum = FuelType;
+  public title = 'Filter list';
+  public formIsValid = false;
 
   constructor(
-    private matDialogRef: MatDialogRef<FleetFilterDialogComponent>,
+    public matDialogRef: MatDialogRef<FleetFilterDialogComponent>,
     private formBuilder: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: {list: object, current: string, option: string}
   ) { }
@@ -42,9 +45,22 @@ export class FleetFilterDialogComponent implements OnInit {
    */
   private initForm() {
     this.form = this.formBuilder.group({
-        filter: [this.data.current],
+        filter: [this.data.current, Validators.required],
         option: [this.data.option]
     });
   }
 
+  public updateSelection($event: MatSelectChange) {
+    // reset selection for brand or fueltype option if main selection is changed
+    this.form.controls.option.setValue(null);
+    if ($event.value !== 'BRAND' && $event.value !== 'FUEL') {
+      this.formIsValid = true;
+    } else {
+      this.formIsValid = false;
+    }
+  }
+
+  public doFilter() {
+    this.matDialogRef.close(this.form.value);
+  }
 }
