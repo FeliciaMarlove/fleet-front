4c93003739ae9158891afc1d6e8cd80d6a75b552
FeliciaMarlove <missflorencemary@hotmail.com>
2021-03-07 09:16:28 +0100

    feat(FleetFilterDialog) Create template for dialog, get list of filters and create select dropdown from it | BEFORE UPGRADING ANGULAR VERSION

--- src/app/fleet-management/fleet/fleet-list/fleet-filter-dialog/fleet-filter-dialog.component.ts
+++ src/app/fleet-management/fleet/fleet-list/fleet-filter-dialog/fleet-filter-dialog.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Brand} from '../../../../shared/enums/brand.enum';
+import {FuelType} from '../../../../shared/enums/fuel-type.enum';
 
 @Component({
   selector: 'app-fleet-filter-dialog',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fleet-filter-dialog.component.scss']
 })
 export class FleetFilterDialogComponent implements OnInit {
+  public form: FormGroup;
+  public filtersList: {key: string, value: string}[] = [];
+  public brandEnum = Brand;
+  public fuelEnum = FuelType;
 
-  constructor() { }
+  constructor(
+    private matDialogRef: MatDialogRef<FleetFilterDialogComponent>,
+    private formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: {list: object, current: string, option: string}
+  ) { }
 
   ngOnInit() {
+    this.initList();
+    this.initForm();
+  }
+
+  /**
+   * Make a list of key:value filters from the plain JSON object
+   */
+  private initList() {
+    Object.entries(this.data.list).forEach(
+      ([key, value]) => {
+        this.filtersList.push({key, value});
+      }
+    );
+  }
+
+  /**
+   * Initialize the form
+   */
+  private initForm() {
+    this.form = this.formBuilder.group({
+        filter: [this.data.current],
+        option: [this.data.option]
+    });
   }
 
 }
