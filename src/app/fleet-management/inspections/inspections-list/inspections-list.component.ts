c11a3a2666aa5138fd77e1565182ad2b4f49b324
FeliciaMarlove <missflorencemary@hotmail.com>
2021-03-07 16:56:52 +0100

    feat(InspectionFilterDialog) + first implementation of Date Picker

--- src/app/fleet-management/inspections/inspections-list/inspections-list.component.ts
+++ src/app/fleet-management/inspections/inspections-list/inspections-list.component.ts
@@ -6,6 +6,9 @@ import {StaffMemberService} from '../../../core/http-services/staff-member.servi
 import {CarService} from '../../../core/http-services/car.service';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
+import {MatDialog} from '@angular/material/dialog';
+import {FleetFilterDialogComponent} from '../../fleet/fleet-list/fleet-filter-dialog/fleet-filter-dialog.component';
+import {InspectionFilterDialogComponent} from './inspection-filter-dialog/inspection-filter-dialog.component';
 
 @Component({
   selector: 'app-inspections-list',
@@ -19,14 +22,21 @@ export class InspectionsListComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   public title = 'Car inspections';
   public paginationChoices: number[] = [10];
+  public filter: string = null;
+  public filterList: object;
+  public option: Date = null;
+  private defaultFilter: string;
 
   constructor(
     private inspectionService: InspectionService,
     private staffService: StaffMemberService,
-    private carService: CarService
+    private carService: CarService,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit() {
+    this.initAvailableFiltersList();
+    this.initDefaultFilter();
     this.initInspectionsList();
   }
 
@@ -35,17 +45,71 @@ export class InspectionsListComponent implements OnInit, AfterViewInit {
   }
 
   /**
-   * Initiate the inspections list with all inspections
+   * Initialize the list of available filters on a key:value base
+   * First filter of the list will be used as default filter when entering the view
+   */
+  private initAvailableFiltersList() {
+    this.filterList = {'From date': 'DATE_ABOVE', Damaged: 'DAMAGED', All: 'ALL'};
+  }
+
+  /**
+   * Initialize the default filter with the key of the first filter in the available filters list
+   * Assign default filter value to filter
+   */
+  private initDefaultFilter() {
+    this.option = this.getTodayMinusOneYear();
+    this.defaultFilter = String(Object.entries(this.filterList).slice(0, 1)[0][1]);
+    this.filter = this.defaultFilter;
+  }
+
+  /**
+   * Get today's date one year ago
+   * Return the date
+   */
+  private getTodayMinusOneYear(): Date {
+    const d = new Date();
+    const lastYear = d.getFullYear() - 1;
+    d.setFullYear(lastYear);
+    return d;
+  }
+
+  /**
+   * Open dialog to choose filtering criteria, passing the available filters list and the current filter to the dialog
+   * Get result after closing the dialog and filter accordingly the list
+   */
+  public doOpenFilterDialog() {
+    this.dialog.open(InspectionFilterDialogComponent, {
+        width: '320px',
+        height: '350px',
+        data: {list: this.filterList, current: this.filter, option: this.option},
+      }
+    ).afterClosed().subscribe(filter => {
+      if (filter) {
+        this.filter = filter.filter;
+        this.option = filter.option;
+        this.initInspectionsList();
+      }
+    });
+  }
+
+  /**
+   * Initiate the inspections list
    */
   private initInspectionsList() {
-    // TODO
-    this.inspectionService.getInspections(null, null).subscribe(
-      inspections => {
-        this.assignCarAndStaffMember(inspections);
-        this.paginationChoices = PaginationListCreatorUtil.setPaginationList(inspections);
-        this.dataSource.data = inspections;
-      }
+    this.inspectionService.getInspections(this.filter, this.option).subscribe(
+      inspections => this.assignInspectionsList(inspections)
     );
+  }
+
+  /**
+   * Assign inspections list to the Data source
+   * Create pagination according to list size and add Staff member and Car to each Inspection in the list
+   * @param cars The list of inspections to display
+   */
+  private assignInspectionsList(inspections) {
+    this.assignCarAndStaffMember(inspections);
+    this.paginationChoices = PaginationListCreatorUtil.setPaginationList(inspections);
+    this.dataSource.data = inspections;
   }
 
   public doOpenInspectionDetail(inspection: any) {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { CarShortDisplayPipe } from './pipe/car-short-display.pipe';
-import { DiscrepancyToDisplayNamePipe } from './pipe/discrepancy-to-display-name.pipe';
-import { BelgianPhoneNumberPipe } from './pipe/belgian-phone-number.pipe';
-import { StaffShortDisplayPipe } from './pipe/staff-short-display.pipe';
+import {CarShortDisplayPipe} from './pipe/car-short-display.pipe';
+import {DiscrepancyToDisplayNamePipe} from './pipe/discrepancy-to-display-name.pipe';
+import {BelgianPhoneNumberPipe} from './pipe/belgian-phone-number.pipe';
+import {StaffShortDisplayPipe} from './pipe/staff-short-display.pipe';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatOptionModule} from '@angular/material/core';
+import {MatNativeDateModule, MatOptionModule} from '@angular/material/core';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatSelectModule} from '@angular/material/select';
 import {MatTableModule} from '@angular/material/table';
@@ -20,17 +20,18 @@ import {MatInputModule} from '@angular/material/input';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import { FuelDisplayPipe } from './pipe/fuel-display.pipe';
-import { NoOptionFilterDialogComponent } from './utils/no-option-filter-dialog/no-option-filter-dialog.component';
+import {FuelDisplayPipe} from './pipe/fuel-display.pipe';
+import {NoOptionFilterDialogComponent} from './utils/no-option-filter-dialog/no-option-filter-dialog.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [
-  CarShortDisplayPipe,
-  DiscrepancyToDisplayNamePipe,
-  BelgianPhoneNumberPipe,
-  StaffShortDisplayPipe,
-  FuelDisplayPipe,
-  NoOptionFilterDialogComponent,
+    CarShortDisplayPipe,
+    DiscrepancyToDisplayNamePipe,
+    BelgianPhoneNumberPipe,
+    StaffShortDisplayPipe,
+    FuelDisplayPipe,
+    NoOptionFilterDialogComponent,
   ],
   imports: [
     CommonModule,
@@ -52,32 +53,37 @@ import { NoOptionFilterDialogComponent } from './utils/no-option-filter-dialog/n
     MatPaginatorModule,
     MatListModule,
     MatSlideToggleModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
-    exports: [
-        RouterModule,
-        MatButtonModule,
-        MatIconModule,
-        MatListModule,
-        MatDialogModule,
-        MatSnackBarModule,
-        FormsModule,
-        MatButtonToggleModule,
-        MatSelectModule,
-        ReactiveFormsModule,
-        MatFormFieldModule,
-        MatOptionModule,
-        MatInputModule,
-        MatSliderModule,
-        MatTableModule,
-        MatListModule,
-        MatPaginatorModule,
-        MatSlideToggleModule,
-        CarShortDisplayPipe,
-        DiscrepancyToDisplayNamePipe,
-        BelgianPhoneNumberPipe,
-        StaffShortDisplayPipe,
-        FuelDisplayPipe,
-        NoOptionFilterDialogComponent
-    ]
+  exports: [
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    FormsModule,
+    MatButtonToggleModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatInputModule,
+    MatSliderModule,
+    MatTableModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatDatepickerModule,
+    MatSlideToggleModule,
+    MatNativeDateModule,
+    CarShortDisplayPipe,
+    DiscrepancyToDisplayNamePipe,
+    BelgianPhoneNumberPipe,
+    StaffShortDisplayPipe,
+    FuelDisplayPipe,
+    NoOptionFilterDialogComponent
+  ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
