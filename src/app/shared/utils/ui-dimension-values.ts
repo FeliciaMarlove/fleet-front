1a8bd6c5bcf98fc6be7b070d13c03ec52f0832d6
FeliciaMarlove <missflorencemary@hotmail.com>
2021-05-10 19:22:22 +0200

    styling - set fields as required to display * next to label

--- src/app/shared/utils/ui-dimension-values.ts
+++ src/app/shared/utils/ui-dimension-values.ts
@@ -1,6 +1,6 @@
 export class UiDimensionValues {
   public static detailsDialogPercentageWidth = '75%';
-  public static detailsDialogPercentageHeight = '85%';
+  public static detailsDialogPercentageHeight = '95%';
   public static filterDialogPixelWidth = '320px';
   public static filterDialogPixelHeight = '350px';
 }
