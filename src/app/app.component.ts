857b445bb45f2f874b4d0dc2d10a17ab92a4aa68
Florence <missflorencemary@hotmail.com>
2021-06-15 11:37:08 +0200

    style(Dark theme switcher) UP AND RUNNING

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,38 @@ export class AppComponent {
   public darkModeUI = false;
 
   constructor(private renderer: Renderer2) {
-    this.renderer.addClass(document.body, this.darkModeUI ? 'dark' : 'light');
+    this.renderPageBodyColor();
   }
 
+  /**
+   * Switch dark/light theme as defined in themes in the styles.scss file and according to value of darkModeUI
+   */
   @HostBinding('class')
   get themeMode() {
     return this.darkModeUI ? 'dark-theme' : 'light-theme';
+  }
+
+  /**
+   * Listen for changes of the toggle state in child component "header"
+   * Change the value of darkModeUI accordingly
+   * Call page body background color rendering
+   * @param $event The boolean emitted by the child component
+   */
+  public getDarkThemeOn($event) {
+    this.darkModeUI = $event;
+    this.renderPageBodyColor();
+  }
+
+  /**
+   * Change the page body background color to match the content background
+   * The name of the class is a reference to a CSS class in the styles.scss file
+   * For example, 'dark' activates the 'body.dark' CSS class
+   * Classes must be explicitly removed before rendering another one
+   * @private
+   */
+  private renderPageBodyColor() {
+    this.renderer.removeClass(document.body, 'dark');
+    this.renderer.removeClass(document.body, 'light');
+    this.renderer.addClass(document.body, this.darkModeUI ? 'dark' : 'light');
   }
 }
