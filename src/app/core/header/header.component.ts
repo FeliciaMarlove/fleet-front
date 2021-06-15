1ad135e494385ee39e98115df2c32688139a0f9d
Florence <missflorencemary@hotmail.com>
2021-06-15 11:37:08 +0200

    style(Dark theme switcher) UP AND RUNNING

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {MatSidenav} from '@angular/material/sidenav';
 import {MatSlideToggle} from '@angular/material/slide-toggle';
 
@@ -13,15 +13,23 @@ export class HeaderComponent implements OnInit {
   private darkThemeIcon = 'nightlight_round';
   private lightThemeIcon = 'wb_sunny';
   public lightDarkToggleIcon = this.darkThemeIcon;
+  @Output() darkThemeOn: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  // checked : false = dark bc true = default
+  /**
+   * Change theme switcher icon to show the icon of the inactive theme (if light theme is on, the icon of dark theme is displayed)
+   * Emit value to parent component to adapt theme and styling
+   * Default position of toggle is "checked" (true) which corresponds to displaying the light theme
+   * When toggle position is false, the dark theme must be displayed
+   * @param toggle The position of the toggle
+   */
   public doToggleLightDark(toggle: MatSlideToggle) {
     console.log('toggle is on position', toggle.checked);
     this.lightDarkToggleIcon = toggle.checked ?  this.darkThemeIcon : this.lightThemeIcon;
+    this.darkThemeOn.emit(!toggle.checked);
   }
 }
