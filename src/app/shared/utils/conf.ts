313f9ed5cd23992d5054f605a6bac77c5ea685f1
Florence <missflorencemary@hotmail.com>
2021-06-27 17:06:46 +0200

    environments(APIEndpoint) dynamically use correct endpoint in dev or prod environment / FIX

--- src/app/shared/utils/conf.ts
+++ src/app/shared/utils/conf.ts
@@ -2,7 +2,7 @@ import {HttpHeaders} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 
 export class Conf {
-  public static SERVER_URL = environment.APIEndpoint;
+  public static SERVER_URL = `${environment.APIEndpoint}`;
   public static httpOptions = {
     headers: new HttpHeaders({'Content-Type': 'application/json'})
   };
