09e711e6ad5a94406cdf905be23b400c087a88c3
Florence <missflorencemary@hotmail.com>
2021-07-03 18:03:56 +0200

    fix(New button) set color to accent

--- src/app/core/azure-services/blob-storage.service.ts
+++ src/app/core/azure-services/blob-storage.service.ts
@@ -1,14 +1,5 @@
 import { Injectable } from '@angular/core';
 import { BlobServiceClient } from '@azure/storage-blob';
-import {KeyValuePipe} from '@angular/common';
-
-@Injectable({
-  providedIn: 'root'
-})
-
-const account = 'tfefleetstorage';
-const sas = 'sp=racwl&st=2021-06-29T16:40:28Z&se=2021-12-31T01:40:28Z&sv=2020-02-10&sr=c&sig=G6uhwfORW%2BanRC1szwArLSg5SZf%2BD5IWT%2BNDpybQHaI%3D';
-const URL = 'https://tfefleetstorage.blob.core.windows.net/';
 
 export enum BLOB_GOAL {
   INSPECTION_IMAGE,
@@ -16,32 +7,32 @@ export enum BLOB_GOAL {
   LOG
 }
 
-export class BlogStorageService {
+const account = 'tfefleetstorage';
+const sas = 'sp=racwl&st=2021-06-29T16:40:28Z&se=2021-12-31T01:40:28Z&sv=2020-02-10&sr=c&sig=G6uhwfORW%2BanRC1szwArLSg5SZf%2BD5IWT%2BNDpybQHaI%3D';
+const URL = 'https://tfefleetstorage.blob.core.windows.net/';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class BlobStorageService {
 
   constructor() { }
 
   /**
-   *
-   * @param containerName
-   * @param goal
-   * @param name
+   * Write block blog to Azure storage and return the url of the saved file
+   * @param containerName Name of the existing Azure container
+   * @param goal The type of file as per internal classification
+   * @param file The file to store
    */
-  public async writeAzureBlob(containerName: string, goal: BLOB_GOAL, name: string, file): Promise<string> {
-    try {
-      const extension = this.checkAndGetFileExtension();
+  public async writeAzureBlockBlob(containerName: string, goal: BLOB_GOAL, file: File): Promise<string> {
+      const extension = file.name.split('.').pop();
       const blobServiceClient = new BlobServiceClient(`https://${account}.blob.core.windows.net/${containerName}?${sas}`);
       const folderName = new Date().getFullYear();
       const containerClient = blobServiceClient.getContainerClient(folderName.toString());
-      const blobName = goal.toString() + name + +new Date() + extension;
+      const blobName = BLOB_GOAL[goal] + '_' + +new Date() + '.' + extension;
       const blockBlobClient = containerClient.getBlockBlobClient(blobName);
       const uploadResponse = await blockBlobClient.upload(file, file.size);
       return URL + containerName + '/' + folderName + '/' + blobName;
-    } catch (e) {
-      return 'ERROR' + e;
-    }
-  }
-
-  private checkAndGetFileExtension() {
-
   }
 }
--- src/app/fleet-management/inspections/inspection-create/inspection-create.component.ts
+++ src/app/fleet-management/inspections/inspection-create/inspection-create.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup} from '@angular/forms';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {InspectionService} from '../../../core/http-services/inspection.service';
 import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
@@ -7,7 +7,7 @@ import {MomentDateAdapter} from '@angular/material-moment-adapter';
 import {Car} from '../../../shared/models/car.model';
 import {CarService} from '../../../core/http-services/car.service';
 import {CarShortDisplayPipe} from '../../../shared/pipe/car-short-display.pipe';
-import { BlobServiceClient, newPipeline } from '@azure/storage-blob';
+import {BLOB_GOAL, BlobStorageService} from '../../../core/azure-services/blob-storage.service';
 
 export const DateFormat = {
   parse: {
@@ -21,13 +21,16 @@ export const DateFormat = {
   }
 };
 
+const azureBlobContainerName = 'inspection';
+const fileReader = new FileReader();
+
 @Component({
   selector: 'app-inspection-create',
   templateUrl: './inspection-create.component.html',
   styleUrls: ['./inspection-create.component.scss'],
   providers: [
-    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
-    { provide: MAT_DATE_FORMATS, useValue: DateFormat }
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: DateFormat}
   ]
 })
 export class InspectionCreateComponent implements OnInit {
@@ -46,8 +49,10 @@ export class InspectionCreateComponent implements OnInit {
     private inspectionService: InspectionService,
     private carService: CarService,
     private carPipe: CarShortDisplayPipe,
+    private blogStorageService: BlobStorageService,
     @Inject(MAT_DIALOG_DATA) public data: { plateNumber }
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     if (!this.plateNumber) {
@@ -89,35 +94,18 @@ export class InspectionCreateComponent implements OnInit {
   }
 
   public async uploadFiles(event) {
-    //TODO logique de l'upload à trigger seulement au send pour éviter écritures inutiles
-// security checkdata type file.type.match('image.*')
+    // TODO logique de l'upload à trigger seulement au send pour éviter écritures inutiles
+    // TODO security checkdata type file.type.match('image.*') AVANT UPLOAD -> v. good practice sécu upload files ?
+    // TODO check if multiple files solution?
+    // TODO TEST sans laisser le blob container public
+    // TODO méthode diff pour up rapport et des images
+    // TODO comment catch les erreurs côté azure proprement ??
     if (event.target.files && event.target.files.length) {
-      console.log(event.target.files[0]);
       const file = event.target.files[0];
       if (file) {
-        console.log(typeof file);
-
-        const fileReader = new FileReader();
         fileReader.readAsDataURL(file);
-        console.log(typeof file);
-
-
-        // // TODO -> créer un service pour gérer l'envoi de fichiers vers Azure Blob
-        // tout mettre dans un try catch
-        const account = 'tfefleetstorage';
-        const sas = 'sp=racwl&st=2021-06-29T16:40:28Z&se=2021-12-31T01:40:28Z&sv=2020-02-10&sr=c&sig=G6uhwfORW%2BanRC1szwArLSg5SZf%2BD5IWT%2BNDpybQHaI%3D';
-        const containerName = 'inspection';
-
-        const blobServiceClient = new BlobServiceClient(`https://${account}.blob.core.windows.net/${containerName}?${sas}`);
-
-        const containerClient = blobServiceClient.getContainerClient(containerName);
-        const blobName = 'go_fuck_yourself'; // ! unique names if N ups
-        const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-        console.log(file);
-        const uploadResponse = await blockBlobClient.upload(file, file.size);
-        // recup url https://tfefleetstorage.blob.core.windows.net/inspection/inspection/go_fuck_yourself
+        const blobUrl = this.blogStorageService.writeAzureBlockBlob(azureBlobContainerName, BLOB_GOAL.INSPECTION_IMAGE, file);
       }
-
     }
   }
 }
