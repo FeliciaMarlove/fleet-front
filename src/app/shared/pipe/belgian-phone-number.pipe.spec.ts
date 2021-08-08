179324eb4ef212f7b0eab4eab4daa9098df9f9d9
Florence <missflorencemary@hotmail.com>
2021-08-08 17:01:49 +0200

    Unit test Karma Jasmine

---
+++ src/app/shared/pipe/belgian-phone-number.pipe.spec.ts
@@ -0,0 +1,14 @@
+import { BelgianPhoneNumberPipe } from './belgian-phone-number.pipe';
+
+describe('Test', () => {
+  const pipe = new BelgianPhoneNumberPipe();
+  it('should create an instance', () => {
+    expect(new BelgianPhoneNumberPipe()).toBeTruthy();
+  });
+  it('should return number with local format', () => {
+    expect(pipe.transform('065347571')).toBe('065 34 75 71');
+  });
+  it('should return number with mobile format', () => {
+    expect(pipe.transform('0475347571')).toBe('0475 34 75 71');
+  });
+});
