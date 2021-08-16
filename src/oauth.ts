c74d55a6131e25c6f7863cbd6b66c95e4fc8925f
Florence <missflorencemary@hotmail.com>
2021-08-16 15:52:04 +0200

    Revert "started adding code for AD"
    This reverts commit d752590ba323f267006c03c12d9ca1057b90f675.
    
    giving up AD

--- src/oauth.ts
+++
@@ -1,9 +0,0 @@
-export const OAuthSettings = {
-  appId: 'd4ea88bd-91fb-4676-929d-68a5ea130704', // TODO variable
-  redirectUri: 'http://localhost:4200',
-  scopes: [
-    'user.read',
-    'mailboxsettings.read',
-    'calendars.readwrite'
-  ]
-};
