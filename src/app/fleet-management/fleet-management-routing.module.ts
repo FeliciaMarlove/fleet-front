220a4ef34b3af39bf284f97d98ebe3c5708cf63f
Florence <missflorencemary@hotmail.com>
2021-09-09 23:23:16 +0200

    no guards

--- src/app/fleet-management/fleet-management-routing.module.ts
+++ src/app/fleet-management/fleet-management-routing.module.ts
@@ -5,15 +5,14 @@ import {FleetListComponent} from './fleet/fleet-list/fleet-list.component';
 import {FillupsListComponent} from './fillups/fillups-list/fillups-list.component';
 import {LeasingCompaniesListComponent} from './leasing-companies/leasing-companies-list/leasing-companies-list.component';
 import {InspectionsListComponent} from './inspections/inspections-list/inspections-list.component';
-import {AuthGuard} from '../core/auth-guard/auth.guard';
 
 const routes: Routes = [
   {path: 'fleet', pathMatch: 'full', component: FleetListComponent},
   {path: '', redirectTo: 'fleet'},
-  {path: 'staff', component: StaffListComponent, canActivate: [AuthGuard]},
-  {path: 'fillups', component: FillupsListComponent, canActivate: [AuthGuard]},
-  {path: 'leasing', component: LeasingCompaniesListComponent, canActivate: [AuthGuard]},
-  {path: 'inspection', component: InspectionsListComponent, canActivate: [AuthGuard]},
+  {path: 'staff', component: StaffListComponent},
+  {path: 'fillups', component: FillupsListComponent},
+  {path: 'leasing', component: LeasingCompaniesListComponent},
+  {path: 'inspection', component: InspectionsListComponent},
   {path: '**', redirectTo: ''}
 ];
 
