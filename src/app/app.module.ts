31d23d9394ffa423a8553343bc6ad1a164dad6b3
Florence <missflorencemary@hotmail.com>
2021-09-09 23:22:13 +0200

    no http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {HeaderComponent} from './core/header/header.component';
 import {SideNavComponent} from './core/side-nav/side-nav.component';
 import {CoreModule} from './core/core.module';
@@ -11,7 +11,6 @@ import {SharedModule} from './shared/shared.module';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {DatePipe, DecimalPipe} from '@angular/common';
 import {BelgianPhoneNumberPipe} from './shared/pipe/belgian-phone-number.pipe';
-import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
@@ -26,11 +25,6 @@ import { AuthModule } from '@auth0/auth0-angular';
     CoreModule,
     SharedModule,
     MatSidenavModule,
-    AuthModule.forRoot({
-      domain: 'dev-882ma-ma.us.auth0.com',
-      clientId: 'gnlwsSnvHI6mZtCxoQjoaa55CmLWw2sV',
-      organization: 'org_xfND0uYxRAI9BIMz'
-    }),
   ],
   providers: [
     DatePipe,
