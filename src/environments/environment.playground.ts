68c67b7a888615f19c3a97bc4a7db034cd457151
Florence <missflorencemary@hotmail.com>
2022-04-12 09:33:24 +0200

    Azure test

--- src/environments/environment.playground.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  APIEndpoint: 'http://fleetapi-play.86f699d3c029485c8755.westeurope.aksapp.io/'
-};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  APIEndpoint: 'http://fleetapi.8f6e75564cde45eeb707.westeurope.aksapp.io/'
+  APIEndpoint: 'https://shewrites-backend.azurewebsites.net/'
 };
